759531255e4872e1e0f96c266234565354536028
arnobert <arnodaeuper@gmx.de>
2023-03-27 16:46:56 +0200

    Fixed frequency calc

--- frequency_calc.py
+++ frequency_calc.py
@@ -1,13 +1,15 @@
 # Calculate the values for Si4032 nominal carrier frequency registers
-#
-# hbsel := 0, fb[4:0] := 19 -> results in 70cm band
+# (!) The chip is not fed by 30 MHz but 26 MHz (!)
+# hbsel := 1, fb[4:0] := 0 -> results in 416..433 MHz
 # Formula from datasheet derived:
 # 
 # f_c = f_TX[MHz] * 6400 - 2752000
 
-f_TX = 432.220
+hbsel = 1
+fb = 0
+f_TX = 432.20
 
-f_c = f_TX * 6400 - 2752000
+f_c = (f_TX / ((26/3) * (hbsel + 1)) - fb - 24)  * 64000
 
 print("Upper byte:")
 print(hex(int(f_c) & 0xFF00))
